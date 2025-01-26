cea77b04ec0b55a9f5a5007c9ff06b89bfc52153
Dr. Ahmed SEJAD <53151316+ahmedsejad1@users.noreply.github.com>
2025-01-26 19:28:19 +0000

    Version 27-01-2025

--- api/api_articles/models.py
+++ api/api_articles/models.py
@@ -5,12 +5,12 @@ from django.db import models
 
 class Article(models.Model):
     ETAT_CHOIX = [
-        ('EN_ATTENTE', 'En attente'),
-        ('VALIDE', 'Validé'),
+        ('En attente', 'En attente'),
+        ('Validé', 'Validé'),
     ]
     titre = models.CharField(max_length=200)
     contenu = models.TextField()
-    etat = models.CharField(max_length=20, choices=ETAT_CHOIX, default='EN_ATTENTE')
+    etat = models.CharField(max_length=20, choices=ETAT_CHOIX, default='En attente')
     date_publication = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
